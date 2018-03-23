37db4974c40f8d99c3c89322b73c6b5e2fe02d63
Joss Giffard-Burley <joss.giffard-burley@wacom.com>
2018-03-23 11:41:22 +0000

    add stuff

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-docker build -t mission-statement-generator . && heroku container:push web -a mission-statement-generator
+dotnet publish -c Release
+heroku container:login
+docker build -t mission-statement-generator  ./bin/Release/netcoreapp1.1/publish
+docker tag mission-statement-generator registry.heroku.com/mission-statement-generator/web
+docker push registry.heroku.com/mission-statement-generator/web
